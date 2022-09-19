e8a0803ea16e43862bd949a5f42076f889fe2620
eveertonrs <78764649+eveertonrs@users.noreply.github.com>
2022-09-19 15:28:43 -0300

    Atualizando os dados

--- src/main/java/br/com/beginner/Main.java
+++ src/main/java/br/com/beginner/Main.java
@@ -1,6 +1,5 @@
 package br.com.beginner;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
@@ -9,41 +8,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
-		
-		
-		int numero = 0;
+
 		System.out.println("Digite : ");
-		for (int i = 0; numero < 4; numero++) {
-			numero = input.nextInt();
+		int numero;
+		numero = input.nextInt();
+
+		switch (numero) {
+		case 1:
+			System.out.println("Amarelo");
+			break;
+		case 2:
+			System.out.println("Vermelho");
+			break;
+		case 3:
+			System.out.println("Azul");
+			break;
+		case 4:
+			System.out.println("Verde");
+			break;
+		case 5:
+			System.out.println("Laranja");
+			break;
+		default:
+			System.out.println("Preto");
+
 		}
-		
-	switch (numero) {
-	case 1:
-		System.out.println("Amarelo");
-		
-		break;
-	case 2:
-		System.out.println("Vermelho");
-		
-		break;
-	case 3:
-		System.out.println("Azul");
-		
-		break;
-	case 4:
-		System.out.println("Verde");
-		
-		break;
-	case 5:
-		System.out.println("Laranja");
-		
-		break;
-	default:
-		System.out.println("Preto");
-	
-	}
-		
-		
+
 	}
 
 }
